41d9409da9c6f350aa8918fa38cedcb7029e0de6
SAMUEL MARCOPITO <samuel2000839@creasp.org.br>
2022-01-14 11:06:01 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'paises',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
